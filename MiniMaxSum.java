97e1ad0c9ac9bf09d016d35a616e7b2fe04e4b98
slava <slava_eclipse>
2019-06-27 19:46:33 +0600

    fix to pass test

--- MiniMaxSum.java
+++ MiniMaxSum.java
@@ -4,15 +4,10 @@ import java.util.List;
 public class MiniMaxSum {
 
 	public static void main(String[] args) {
-
-		int[] arr = new int[] { 1, 3, 5, 7, 9 };
-
+		int[] arr = new int[] {256741038, 623958417, 467905213, 714532089, 938071625};
 		miniMaxSum(arr);
-	
 	}
-	
-	
-	
+
 	public static void miniMaxSum(int[] arr) {
 		List<Integer> minNumbersArr = new ArrayList<>();
 		List<Integer> maxNumbersArr = new ArrayList<>();
@@ -21,15 +16,11 @@ public class MiniMaxSum {
 			minNumbersArr.add(arr[i]);
 			maxNumbersArr.add(arr[i]);
 		}
-		
+
 		long minRes = minNumbersArr.stream().mapToInt(v -> v).sum() - minNumbersArr.stream().mapToInt(v -> v).max().getAsInt();
 		int maxRes = maxNumbersArr.stream().mapToInt(v -> v).sum() - maxNumbersArr.stream().mapToInt(v -> v).min().getAsInt();
-		
+
 		System.out.println(minRes + " " + maxRes);
 	}
-	
-	
-	
-	
 
 }
